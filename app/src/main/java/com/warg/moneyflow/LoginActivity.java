c3fec18db5557d2f6b8f6297887a11aead16c123
TesterovVasiliy <cqk87020@laoeq.com>
2016-04-22 20:57:30 +0300

    Update LoginActivity.java

--- app/src/main/java/com/warg/moneyflow/LoginActivity.java
+++ app/src/main/java/com/warg/moneyflow/LoginActivity.java
@@ -38,7 +38,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-
+//привет мир
     /**
      * Id to identity READ_CONTACTS permission request.
      */
