b8be1128f668bd66a77f84f46445632ae9f65321
rash1k <rash1k-1@mail.ru>
2016-04-22 20:46:29 +0300

    Update Prefs.java
    Change

--- app/src/main/java/com/android11/android/moneyflow/util/Prefs.java
+++ app/src/main/java/com/android11/android/moneyflow/util/Prefs.java
@@ -5,7 +5,7 @@ public class Prefs {
     public static final String LOG_TAG = "Money_Flow";
     public static final String ID_FIELD_NAME = "_id";
 
-    public static final String TEST = "Test";
+    public static final String TEST = "TEST";
 
     public static final String FN_FIELD_NAME = "first_name";
     public static final String LN_FIELD_NAME = "last_name";
