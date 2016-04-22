b0847603fa85c6e9dac95192a7a0c509cd2c1d5c
Test <cqk87020@laoeq.com>
2016-04-22 20:41:17 +0300

    3

---
+++ app/src/main/java/com/warg/moneyflow/util/Prefs.java
@@ -0,0 +1,17 @@
+package com.warg.moneyflow.util;
+
+/**
+ * Created by Test on 13.04.2016.
+ */
+public class Prefs {
+    public static final String LOG_TAG = "MoneyFlow";
+
+    public static final String ID_FIELD_NAME = "_id";
+
+    public static final String FN_FIELD_NAME = "first_name";
+    public static final String LN_FIELD_NAME = "last_name";
+    public static final String BIRTHDAY_FIELD_NAME = "birthday";
+    public static final String EMAIL_FIELD_NAME = "email";
+
+    public static final String API_SERVER = "http://cityfinder.esy.es";
+}
