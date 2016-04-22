1b0d46542861c489fc9abf15a83f1ed5c70da0b9
Dmitriy Ostrovskiy <rash1k-1@mail.ru>
2016-04-22 20:16:03 +0300

    Initial commit

---
+++ app/src/main/java/com/android11/android/moneyflow/util/Prefs.java
@@ -0,0 +1,15 @@
+package com.android11.android.moneyflow.util;
+
+public class Prefs {
+
+    public static final String LOG_TAG = "Money_Flow";
+    public static final String ID_FIELD_NAME = "_id";
+
+    public static final String FN_FIELD_NAME = "first_name";
+    public static final String LN_FIELD_NAME = "last_name";
+    public static final String BIRTHDAY_FIELD_NAME = "birthday";
+    public static final String EMAIL_FIELD_NAME = "email";
+    public static final String API_SERVER = "http://cityfinder.esy.es";
+
+//    public static final Uri API_URI = Uri.parse("content://"+AUTORITY+"/"+PATH_API);
+}
